a0e8171ce2cccd3795f9c7ada53252531f0f34be
Lucas Guedon <guedon.lucas@laposte.net>
2021-11-10 11:57:31 +0100

    Change optimizer to adam

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ z = Dense(compressed_size, activation="relu")(layer)
 encoder = keras.Model(e_input_layer, z, name="encoder")
 
 encoder.summary()
-encoder.compile(optimizer=optimizers.SGD(learning_rate=0.1), loss="mse", metrics=["mae"])
+encoder.compile(optimizer=optimizers.Adam(learning_rate=0.002), loss="mse", metrics=["mae"])
 
 d_input_layer = keras.Input(shape=(compressed_size,))
 layer = layers.Dense(conv_output.shape[1] * conv_output.shape[2] * conv_output.shape[3], activation="relu")(d_input_layer)
@@ -44,20 +44,20 @@ layer = Conv2DTranspose(1, 3, activation="relu", strides=2, padding="same")(laye
 decoder = keras.Model(d_input_layer, layer, name="decoder")
 
 decoder.summary()
-decoder.compile(optimizer=optimizers.SGD(learning_rate=0.1), loss="mse", metrics=["mae"])
+decoder.compile(optimizer=optimizers.Adam(learning_rate=0.002), loss="mse", metrics=["mae"])
 
 autoencoder = keras.Model(e_input_layer, decoder(z), name="auto-encoder")
 
 autoencoder.summary()
-autoencoder.compile(optimizer=optimizers.SGD(learning_rate=0.1), loss="mse", metrics=["mae"])
+autoencoder.compile(optimizer=optimizers.Adam(learning_rate=0.002), loss="mse", metrics=["mae"])
 
 def load_files():
     files = np.empty((0, 28, 28, 1))
     for filename in os.listdir(directory):
         if filename.endswith('.npy') :
             print(directory + '/' + filename)
-            f = np.load(directory + '/' + filename, mmap_mode='r')
-            files = np.append(files, f.reshape((f.shape[0], 28, 28, 1)) / 255.0, axis=0)
+            f = np.load(directory + '/' + filename, mmap_mode='r') / 255.0 # normalize to [0.0, 1.0] range
+            files = np.append(files, f.reshape((f.shape[0], 28, 28, 1)), axis=0)
     print("Files loaded")
     np.random.shuffle(files)
     return files
